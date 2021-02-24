466015c9f5587ea8b193ae2d42ec7bc7739f3f35
Vladimir V. Vladimirov <mckeency@gmail.com>
2021-02-24 22:12:08 +0300

    added Easy-IntersectionOfTwoArraysII.py

---
+++ FLG/week02/Easy-IntersectionOfTwoArraysII/Easy-IntersectionOfTwoArraysII.py
@@ -0,0 +1,71 @@
+"""
+350. Intersection of Two Arrays II
+
+Runtime: 368 ms, faster than 5.62% of Python3 online submissions for Intersection of Two Arrays II.
+Memory Usage: 14.4 MB, less than 71.44% of Python3 online submissions for Intersection of Two Arrays II.
+
+"""
+
+
+class Solution:
+    def insert_sort(self, a):
+        """ Insertion Sort implementation"""
+        n = len(a)
+        for i in range(n):
+            for j in range(i, 0, -1):
+                if a[j] <= a[j - 1]:
+                    a[j], a[j - 1] = a[j - 1], a[j]
+                else:
+                    break
+
+
+    def intersect(self, nums1, nums2):
+        self.insert_sort(nums1)
+        self.insert_sort(nums2)
+
+        # Choose the biggest array
+        if len(nums1) >= len(nums2):
+            n1,n2 = len(nums1),len(nums2)
+            arr1,arr2 = nums1,nums2
+        else:
+            n1,n2 = len(nums2),len(nums1)
+            arr1,arr2 = nums2,nums1
+
+        res = []
+        cur_i = cur_j = 0
+        for i in range(cur_i, n1):
+            for j in range(cur_j, n2):
+                if cur_i == n1:                 # for case i+1 = len(arr1) and error arr1[n1]
+                    break
+                if arr1[cur_i] == arr2[cur_j]:
+                    res.append(arr1[cur_i])
+                    cur_i += 1
+                    cur_j += 1
+                    break
+                elif arr1[cur_i] > arr2[cur_j]:
+                    cur_j += 1
+                else:
+                    cur_i += 1
+
+        return res
+
+
+def test_intersect():
+    inp = [[[1,2,2,1],[2,2]],
+           [[4,9,5], [9,4,9,8,4]],
+           [[4, 7, 9, 7, 6, 7], [5, 0, 0, 6, 1, 6, 2, 2, 4]],
+           [[54,93,21,73,84,60,18,62,59,89,83,89,25,39,41,55,78,27,65,82,94,61,12,38,76,5,35,6,51,48,61,0,47,60,84,9,13,28,38,21,55,37,4,67,64,86,45,33,41],
+            [17, 17, 87, 98, 18, 53, 2, 69, 74, 73, 20, 85, 59, 89, 84, 91, 84, 34, 44, 48, 20, 42, 68, 84, 8, 54, 66, 62, 69, 52, 67, 27, 87, 49, 92, 14, 92, 53, 22, 90, 60, 14, 8, 71, 0, 61, 94, 1, 22, 84, 10, 55, 55, 60, 98, 76, 27, 35, 84, 28, 4, 2, 9, 44, 86, 12, 17, 89, 35, 68, 17, 41, 21, 65, 59, 86, 42, 53, 0, 33, 80, 20]
+            ]
+           ]
+    out = [[2,2], [4,9], [4,6],
+           [0, 4, 9, 12, 18, 21, 27, 28, 33, 35, 41, 48, 54, 55, 55, 59, 60, 60, 61, 62, 65, 67, 73, 76, 84, 84, 86, 89, 89, 94]
+           ]
+    sol = Solution()
+    for k in range(len(inp)):
+        test_res = sol.intersect(inp[k][0], inp[k][1])
+        print("Test", k + 1, ":", "OK" if test_res == out[k] else "Failed")
+        print()
+
+if __name__ == '__main__':
+    test_intersect()
