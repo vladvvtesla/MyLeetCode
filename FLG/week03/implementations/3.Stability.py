20750025475654d8afe203b25bcbe9467447d136
Vladimir V. Vladimirov <mckeency@gmail.com>
2021-03-01 16:58:08 +0300

    3.Stability.py

---
+++ FLG/week03/implementations/3.Stability.py
@@ -0,0 +1,14 @@
+"""
+
+A typical application: first sort by name, then sort by section
+
+Q. Which sort are stable?
+A. Insertion sort and mergesort (but not selection sort or shellsort)
+
+Note Need to carefully check code ("less than" vs "less than or equal to " )
+
+Quicksort is an efficient, but unstable, sorting algorithm
+
+Mergesort is an efficient and stable sorting algorithm
+
+"""
