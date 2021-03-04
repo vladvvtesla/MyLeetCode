69fddb740c04d6ca33788b29270d21141f59e21f
Vladimir V. Vladimirov <mckeency@gmail.com>
2021-03-04 10:58:24 +0300

    added 4.imp-3-way-quicksort.py

---
+++ FLG/week03/implementations/4.imp-3-way-quicksort.py
@@ -0,0 +1,115 @@
+"""
+3-way quicksort: Python implementation
+
+Duplicate keys
+Mergesort with duplicate keys. Always between  1/2 NlgN and NlgN compares
+
+Quicksort with duplicate keys. The classic algorithm goes quadratic unless partitioning stops on equal keys
+Recommended: Stop scan on items equal to the partitioning item
+Consequence: ~NlgN compares when all keys equal
+
+Bottom line: Randomized quicksort with 3-way partitioning reduces running time from linearithmic to linear in a broad class of applications.
+Quicksort with 3-way partitioning is most effective when the input array has a few distinct items?
+
+
+Dijkstra 3-way partitioning
+
+Engineering a system sort
+Basic algorithm - quick sort
+ - Cutoff to insertion sort for small subarrays
+ - Partitioning scheme: 3-way partitioning
+ - Partitioning items
+      Large arrays: middle entry
+      medium arrays: median of 3
+      large arrays: Tukey's ninther
+"""
+
+def quicksort(a, lo, hi):
+    """
+    3-way quicksort: Python implementation
+    :param a:  Array
+    :param lo: low Index
+    :param hi: high index
+    """
+    print('a:', a)
+
+    import random
+    random.shuffle(a)            # Shuffle array a
+    print('shuffled a:', a)
+
+    if hi <= lo:
+        return
+    lt, gt = lo, hi
+    i = lo
+    while i <= gt:
+        if a[i] < a[lo]:
+            lt,i = i,lt
+            lt += 1
+            i += 1
+        elif a[i] > a[lo]:
+            gt,i = i,gt
+            gt -= 1
+        else:
+            i += 1
+        print('i:', i)
+        print('lo:', lo)
+        print('lt:', lt)
+        print('gt:', gt)
+        print('a:', a)
+
+    quicksort(a, lo, lt - 1)
+    quicksort(a, gt + 1, hi)
+
+    # assert isSorted(a)            # Postcondition: a is sorted
+
+
+def isSorted(a, asc=True):
+    """
+    Check if an array is sorted
+    :param a: array
+    :param asc: ascending or descending
+    :return: boolean
+    """
+    s = 2*int(asc) - 1      # sign:   int(True)=1 => 2*1-1 = 1
+
+    for i in range(len(a)-1):
+        if s * a[i] > s * a[i+1]:
+            return False
+    return True
+
+
+def test_quicksort():
+    a = [6,2,9,2,5]
+    a_sorted = [2,2,5,6,9]
+    quicksort(a, 0, len(a)-1)
+    print("Test", "1.", "OK" if a == a_sorted else "Failed")
+    print()
+
+    b = [1,0,0,0,0]
+    b_sorted = [0,0,0,0,1]
+    quicksort(b, 0, len(a)-1)
+    print("Test", "2.", "OK" if b == b_sorted else "Failed")
+    print()
+
+    c = [1]
+    b_sorted = [1]
+    quicksort(b, 0, len(a)-1)
+    print("Test", "3.", "OK" if b == b_sorted else "Failed")
+    print()
+
+
+def test_isSorted():
+    inp = [[0, 1, 2 , 3, 4], [6, 2, 9, 2, 5]]
+    out = [True, False]
+    for i in range(len(inp)):
+        test_res = isSorted(inp[i])
+        print("Test", i + 1, ":", "OK" if test_res == out[i] else "Failed")
+        print()
+
+
+if __name__ == '__main__':
+    print('test_quicksort')
+    test_quicksort()
+
+    print('test_isSorted')
+    test_isSorted()
