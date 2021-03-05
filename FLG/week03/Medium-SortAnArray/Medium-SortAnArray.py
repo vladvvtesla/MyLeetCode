06fd426af6e11655977bf3a063362940222d02cf
Vladimir V. Vladimirov <mckeency@gmail.com>
2021-03-05 10:11:51 +0300

    modified Medium-SortAnArray.py

--- FLG/week03/Medium-SortAnArray/Medium-SortAnArray.py
+++ FLG/week03/Medium-SortAnArray/Medium-SortAnArray.py
@@ -2,87 +2,46 @@
 912. Sort an Array
 
 Solution
-1) sort array using mergesort in ascending order
+1) sort array using quicksort in ascending order
 
 Time Complexity:  O( N * lg N )
                   sorting array       O( N * lg N )
-Space Complexity: O(N) plus extra space proportional to N
+Space Complexity: O(N) in-place
 
-Success:
-Runtime: 532 ms, faster than 7.24% of Python3 online submissions for Sort an Array.
-Memory Usage: 20.5 MB, less than 48.29% of Python3 online submissions for Sort an Array.
+Success, but too slow
+Runtime: 360 ms, faster than 39.45% of Python3 online submissions for Sort an Array.
+Memory Usage: 20 MB, less than 88.56% of Python3 online submissions for Sort an Array.
 """
 
 class Solution:
-    def __init__(self):
-        pass
-
-    def mergesort(self, a):
-        """ Merging Sort implementation"""
-        if len(a) <= 1:
-            return                  # in-place sorting, so just return None
-        middle = len(a) // 2
-        l = [a[i] for i in range(0, middle)]       # Left half of the list
-        r = [a[i] for i in range(middle, len(a))]  # Right half of the list
-        self.mergesort(l)
-        self.mergesort(r)
-        c = self.merge(l, r)     # merge two array
-        for i in range(len(a)):  # новый отсортированный список скопировать в изначальный
-             a[i] = c[i]
-
-    def merge(self, a: list, b: list):
+    def quicksort(self, a, lo, hi):
         """
-        Merging of two sorted arrays
-        :param a:   firts sorted array
-        :param b:   second sorted array
-        :return c:  sorted array after merging
+        3-way quicksort: Python implementation
+        :param a:  Array
+        :param lo: low Index
+        :param hi: high index
         """
-        # assert self.isSorted(a)         # Precondition: is sorted
-        # assert self.isSorted(b)         # Precondition: is sorted
-
-        if a[-1] <= b[0]: c = a + b     # Stop if already sorted:
-
-        c = [0] * (len(a) + len(b))
-        i = k = n = 0                   # indexes for arrays a, b, c
-        while i < len(a) and k < len(b):
-            if a[i] <= b[k]:
-                c[n] = a[i]
+        if hi <= lo:
+            return
+        lt, gt = lo, hi
+        i = lo
+        while i <= gt:
+            if a[i] < a[lt]:
+                a[lt], a[i] = a[i], a[lt]
+                lt += 1
                 i += 1
-                n += 1
+            elif a[i] > a[lo]:
+                a[gt], a[i] = a[i], a[gt]
+                gt -= 1
             else:
-                c[n] = b[k]
-                k += 1
-                n += 1
-        while i < len(a):  # insert tail of a into c, if it exists
-            c[n] = a[i]
-            i += 1
-            n += 1
-        while k < len(b):
-            c[n] = b[k]  # insert tail of b into c, if it exists
-            k += 1
-            n += 1
+                i += 1
 
-        # assert self.isSorted(c)  # Postcondition: c is sorted
-
-        return c
-
-    def isSorted(self, a, asc=True):
-        """
-        Check if an array is sorted
-        :param a: array
-        :param asc: ascending or descending
-        :return: boolean
-        """
-        s = 2 * int(asc) - 1  # sign:   int(True)=1 => 2*1-1 = 1
-
-        for i in range(len(a) - 1):
-            if s * a[i] > s * a[i + 1]:
-                return False
-        return True
+        self.quicksort(a, lo, lt - 1)
+        self.quicksort(a, gt + 1, hi)
 
     def sortArray(self, nums):
-        self.mergesort(nums)
-        return nums                # Leetcode needs it
+        self.quicksort(nums, 0, len(nums)-1)
+        return nums                           # Leetcode needs it
 
 
 def test_sortArray():
