b7caf7eb74b313d78cbcf7d00b2f67668973b3bc
Vladimir V. Vladimirov <mckeency@gmail.com>
2021-03-11 17:45:51 +0300

    modified Easy-Stack-RemoveOutermostParentheses.py

--- FLG/week03a/Easy-Stack-RemoveOutermostParentheses/Easy-Stack-RemoveOutermostParentheses.py
+++ FLG/week03a/Easy-Stack-RemoveOutermostParentheses/Easy-Stack-RemoveOutermostParentheses.py
@@ -1,6 +1,15 @@
 """
 1021. Remove Outermost Parentheses (Easy, Stack)
 
+Main idea: We use LinkedListQueue
+1. Enqueue all parentheses to Queue
+2. Calculate a summ:   '(' is +1, ')' is -1.
+3. If summ == 0, dequeue all parentheses from Queue, and append them into List,
+excluding the fist and the last parentheses. "(()())" => "()()"
+4 continue for all parentheses in the input string
+
+Runtime: 104 ms, faster than 6.32% of Python3 online submissions for Remove Outermost Parentheses.
+Memory Usage: 14.3 MB, less than 64.17% of Python3 online submissions for Remove Outermost Parentheses.
 
 """
 
@@ -8,29 +17,6 @@ class Node(object):
     def __init__(self, item=None):
         self.item = item
         self.next = None
-
-class LinkedListStack():
-    """" Stack Implementation using Singly-Linked Lists.  Coursera Sedgewick 1 w2 """
-    def __init__(self, first=None):
-        self.first = first
-
-    def isEmpty(self):
-        """ Chek if the first node is Null"""
-        return self.first == None
-
-    def push(self, item):
-        """ Push to the beginning of Linked List """
-        oldfirst = self.first         # Save link to the old head into a tmp variable
-        self.first = Node()           # new first node
-        self.first.item = item        #
-        self.first.next = oldfirst    # create link to the
-
-    def pop(self):
-        """ Pop from the beginning of Linked List"""
-        item = self.first.item         # Save link to the old head into a tmp variable
-        self.first = self.first.next   # new first node
-        return item
-
 
 class LinkedListQueue():
     def __init__(self):
@@ -45,7 +31,6 @@ class LinkedListQueue():
         """ Push element x onto end of queue """
         oldlast = self.last
         self.last = Node(item)
-        print(self.last.item)
         if self.isEmpty():                   # special case for empty queue
             self.first = self.last
         else:
@@ -61,81 +46,43 @@ class LinkedListQueue():
 
 class Solution:
     def __init__(self):
-        self.inp = LinkedListStack()  # Stack for input sting
-        self.res = LinkedListQueue()  # Queue for result
-        self.sum = 0                  #  '(' is +1, ')' is -1.
-        self.res_string = ''
+        self.inp = LinkedListQueue()   # Queue for input sting
+        self.sum = 0                   #  '(' is +1, ')' is -1.
+        self.res_list = []
 
     def removeOuterParentheses(self, s):
         """
         Removing the outermost parentheses
-        :param s:     # input string
-        :return:
+        :param s:     input string
+        :return:      string
         """
         for p in s:
             if p == '(':
-                self.inp.push(p)
+                self.inp.enqueue(p)
                 self.sum += 1
-                print('self.sum', self.sum)
             elif p == ')':
-                self.inp.push(p)
+                self.inp.enqueue(p)
                 self.sum -= 1
-                print('self.sum', self.sum)
 
             if self.sum == 0:
-                print('self.sum again', self.sum)
-                self.inp.pop()                    # delete last ')' from '(())'
-                while not self.inp.isEmpty():     # replace all from inp to res queue'(()'
-                    tmp = self.inp.pop()
-                    print('tmp', tmp)
-                    if not self.inp.isEmpty():    # don't enqueue last '('  from '(()' => res = '()'
-                        self.res.enqueue(tmp)
+                self.inp.dequeue()                # delete ferst ')' from '(())'
+                while not self.inp.isEmpty():     # replace all from inp to res list'()'
+                    tmp = self.inp.dequeue()
+                    if not self.inp.isEmpty():    # don't append last ')'  from '(())' => res = '()'
+                        self.res_list.append(tmp)
 
-        # We should reverse order, so lets replace all queue to stack
-        tmp = LinkedListStack()
-        while not self.res.isEmpty():
-            tmp.push(self.res.dequeue())
-
-
-        # Make string from res stack
-        # while not self.res.isEmpty():
-            # print('self.res.dequeue()', self.res.dequeue())
-            # self.res_string += self.res.dequeue()
-
-        while not tmp.isEmpty():
-            # print('self.res.dequeue()', self.res.dequeue())
-            self.res_string += tmp.pop()
-
-        return self.res_string
+        return ''.join(self.res_list)
 
 
 def test_removeOuterParentheses():
-    """
-    test queue
-    :return:
-    """
-    # print('start my test')
-
-    # res = LinkedListQueue()
-    # res.enqueue('(')
-    # res.enqueue(')')
-    # print('first dequeue', res.dequeue())
-    # print('second dequeue', res.dequeue())
-
-
-    # print('end my test')
-
-    inp = ["(()())(())(()(()))"]
-    out = ["()()()()(())"]
-
-    #inp = ["(()())(())",
-    #       "(()())(())(()(()))",
-    #       "()()",
-    #       "(())"]
-    #out = ["()()()",
-    #       "()()()()(())",
-    #       "",
-    #       "()"]
+    inp = ["(()())(())",
+           "(()())(())(()(()))",
+           "()()",
+           "(())"]
+    out = ["()()()",
+           "()()()()(())",
+           "",
+           "()"]
     for i in range(len(inp)):
         sol = Solution()
         test_res = sol.removeOuterParentheses(inp[i])
