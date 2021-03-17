8e9c200e8c33ccff8fe8ed2a3be92fe8af9d83e9
Vladimir V. Vladimirov <mckeency@gmail.com>
2021-03-17 11:07:02 +0300

    modified week04 implementation

---
+++ FLG/week04/week04_problems.py
@@ -0,0 +1,14 @@
+"""
+Задачи на 13.03.2021 - 19.03.2021
+
+Problem                                        | Difficulty | Notes
+
+703.Kth Largest Element in a Stream            | Easy
+1046. Last Stone Weight                        | Easy
+692. Top K Frequent Words                      | Medium
+617. Merge Two Binary Trees                    | Easy
+98. Validate Binary Search Tree                | Medium
+1038. Binary Search Tree to Greater Sum Tree   | Medium
+230. Kth Smallest Element in a BST             | Medium
+"""
+
