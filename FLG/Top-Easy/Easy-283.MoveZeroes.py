c85d12a9c811dc7948af59edaef819518d1da90b
Vladimir V. Vladimirov <mckeency@gmail.com>
2021-06-04 15:19:15 +0300

    added comments Easy-283.MoveZeroes.py

--- FLG/Top-Easy/Easy-283.MoveZeroes.py
+++ FLG/Top-Easy/Easy-283.MoveZeroes.py
@@ -1,23 +1,33 @@
+"""
+
+283. Easy - Move Zeroes
+
+Изначально считаем, что нужно меняться с индексом 0
+Делаем для всех элементов arr:
+Если встертили 0,
+    то ничего не делаем, то есть
+    с кем меняться остается по прежнему с индексом 0
+если встретили не 0,
+    то  1) перемена местами arr[metka] и arr[curr]
+        2) увеличиваем с кем меняться на 1
+
+таким образом после всех перестановок местами, у нас все 0 окажутся в конце массива,
+Do not return anything, modify nums in-place instead.
+
+Time Complexity:   O(n)
+Space Complexity:  O(n)
+
+Runtime: 48 ms, faster than 75.03% of Python3 online submissions for Move Zeroes.
+Memory Usage: 15.4 MB, less than 64.69% of Python3 online submissions for Move Zeroes.
+"""
+
 class Solution:
     def moveZeroes(self, nums):
-        """
-        Изначально считаем, что нужно меняться с индексом 0
-        Делаем для всех элементов arr:
-        Если встертили 0,
-            то ничего не делаем, то есть
-            с кем меняться остается по прежнему с индексом 0
-        если встретили не 0,
-            то  1) перемена местами arr[metka] и arr[curr]
-                2) увеличиваем с кем меняться на 1
-
-        таким образом после всех перестановок местами, у нас все 0 окажутся в конце массива,
-        Do not return anything, modify nums in-place instead.
-        """
         x_idx = 0          # С кем меняться
-        for k,j in enumerate(nums):
-            if j != 0:
-                nums[k], nums[x_idx] = nums[x_idx],nums[k]
-                x_idx += 1
+        for k,j in enumerate(nums):                             # O(n)
+            if j != 0:                                          # O(1)
+                nums[k], nums[x_idx] = nums[x_idx],nums[k]      # O(1)
+                x_idx += 1                                      # O(1)
 
         return nums    # Для leetcode это не нужно, только для теста
 
