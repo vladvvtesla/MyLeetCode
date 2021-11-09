cbfe5e09e1485381b174b0f2182c1a4dca008f4f
Vladimir V. Vladimirov <mckeency@gmail.com>
2021-11-09 16:12:15 +0300

    25.HeapSort.py

---
+++ FLG/Implementations/Sorting/25.HeapSort.py
@@ -0,0 +1,76 @@
+"""
+Heap Sort
+
+The insert method is called n number of times. Together with the float method, the
+insert operation takes a worst case runtime of O(n log n), as does the pop method. As
+such, this sorting algorithm incurs a worst case runtime of O(n log n).
+"""
+
+
+class MinHeap:
+    def __init__(self):
+        self.heap = [0]
+        self.size = 0
+
+    def float(self, k):
+        """
+        после того, как мы новый элемент поставили в самую нижнюю ноду,
+        Нам нужно поднять этот элемент и поставить на место, чтобы соблюсти условие Min Heap
+        """
+        while k // 2 > 0:
+            if self.heap[k] < self.heap[k // 2]:
+                self.heap[k], self.heap[k // 2] = self.heap[k // 2], self.heap[k]
+            k //= 2
+
+    def minindex(self, k):
+        """
+        после того, как мы самый нижний элемент сделали root,
+        нам нужно этот элемент спустить вниз и поставить на место
+        У нас два выбора, спустить его на место правого child, или на место левого.
+        Поэтому этот метод minindex и сравнивает правый и левый child,
+        """
+        if k * 2 + 1 > self.size:
+            return k * 2
+        elif self.heap[k * 2] < self.heap[k * 2 + 1]:
+            return k * 2
+        else:
+            return k * 2 + 1
+
+    def sink(self, k):
+        while k * 2 <= self.size:
+            mi = self.minindex(k)
+            if self.heap[k] > self.heap[mi]:
+                self.heap[k], self.heap[mi] = self.heap[mi], self.heap[k]
+            k = mi
+
+    def insert(self, item):
+        self.heap.append(item)
+        self.size += 1
+        self.float(self.size)
+
+    def pop(self):
+        item = self.heap[1]
+        self.heap[1] = self.heap[self.size]
+        self.size -= 1
+        self.heap.pop()
+        self.sink(1)
+        return item
+
+    def heap_sort(self):
+        sorted_list = []
+        for node in range(self.size):
+            n = self.pop()
+            sorted_list.append(n)
+        return sorted_list
+
+
+if __name__ == '__main__':
+    h = MinHeap()
+    unsorted_list = [4, 8, 7, 2, 9, 10, 5, 1, 3, 6]
+    for i in unsorted_list:
+        h.insert(i)
+    print("Unsorted list:", unsorted_list)
+
+    sorted_list = h.heap_sort()
+    print("Sorted list:", sorted_list)
+
