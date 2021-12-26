e35043e669bec2951298bab7e4cdfc1490013aba
Vladimir V. Vladimirov <mckeency@gmail.com>
2021-12-26 09:26:52 +0300

    Modified 16.DepthFirstSearch.py

--- FLG/Implementations/Graphs/16.DepthFirstSearch.py
+++ FLG/Implementations/Graphs/16.DepthFirstSearch.py
@@ -33,28 +33,28 @@ components, and finding the bridges of a graph, among others.
 
 
 def dfs(graph, root):
-    visited_vertices = list()
+    visited = list()
     graph_stack = list()
     graph_stack.append(root)
     node = root
 
     while len(graph_stack) > 0:
-        if node not in visited_vertices:
-            visited_vertices.append(node)
+        if node not in visited:
+            visited.append(node)
         adj_nodes = graph[node]
-        if set(adj_nodes).issubset(set(visited_vertices)):
+        if set(adj_nodes).issubset(set(visited)):
             graph_stack.pop()
             if len(graph_stack) > 0:
                 node = graph_stack[-1]
             continue
         else:
-            remaining_elements = set(adj_nodes).difference(set(visited_vertices))
+            remain_els = set(adj_nodes).difference(set(visited))
 
-        first_adj_node = sorted(remaining_elements)[0]
+        first_adj_node = sorted(remain_els)[0]
         graph_stack.append(first_adj_node)
         node = first_adj_node
 
-    return visited_vertices
+    return visited
 
 
 if __name__ == '__main__':
